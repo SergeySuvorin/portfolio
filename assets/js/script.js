ffe008ec952f8b777658d0d6cdbdfab90da90f00
Sergey Suvorin <82051311+SergeySuvorin@users.noreply.github.com>
2021-06-26 01:15:33 +0300

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+//Burger-menu
+function burgerMenu(selector){
+	let menu = $(selector);
+	let button = menu.find('.burger-menu__button');
+	let links = menu.find('.burger-menu__link')
+	button.on('click', (e)=>{
+		e.preventDefault();
+		toggleMenu();
+	})
+
+	links.on('click', ()=>
+		toggleMenu());
+function toggleMenu(){
+	menu.toggleClass('burger-menu-active');
+
+	if(menu.hasClass('burger-menu-active')){
+		$('body').css('overflow', 'hidden');
+		}else{
+			$('body').css('overflow', 'visible');
+		}
+	}
+}
+burgerMenu('.burger-menu');
+
